53967dadebf128ae5bc64f07240878d0c43f30c9
PabloCamilo07 <pablo.aquino@aluno.senai.br>
2024-01-26 13:05:20 -0300

    .

--- carousel/begginpage.php
+++ carousel/begginpage.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
@@ -11,7 +11,7 @@
 
   <!-- LINK BOOSTSTRAP JS-->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  
+
   <link rel="stylesheet" href="style-begginpage.css">
   <title>Document</title>
 </head>
@@ -24,10 +24,10 @@
   <div id="carouselExample" class="carousel slide">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/imagem1.jpg" class="d-block w-100" alt="">
+      <img src="img/imagem 1.jpg" class="d-block w-100" alt="">
     </div>
     <div class="carousel-item">
-      <img src="img/imagem2.jpg" class="d-block w-100" alt="">
+      <img src="img/imagem 2.jpg" class="d-block w-100" alt="">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
