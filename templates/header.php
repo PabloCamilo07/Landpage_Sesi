ad35eb7affc6362505c6272ff8862bb7caf7037c
xXBertholdoXx <131973725+xXBertholdoXx@users.noreply.github.com>
2024-01-26 14:26:06 -0300

    .scsdsd
    sdssdsd

---
+++ templates/header.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portal Sesi</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+
+  <header>
+    <nav class="navbar" style="background-color: #BB0102;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#" style="color: aliceblue;">
+          <img src="../img/logosesi.png" alt="Logo" width="200" height="66" class="d-inline-block align-text-top">
+          <br>Serviço Social da Indústria
+        </a>
+
+        <nav class="nav justify-content-center">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">
+              <i class="bi bi-search"></i>
+            </span>
+            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+          </div>
+        </nav>
+
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+          <i class="bi bi-person-circle"></i>
+          </li>
+          <li class="nav-item">
+          <i class="bi bi-box-arrow-right"></i>
+          </li>
+        </ul>
+
+      </div>
+    </nav>
+
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" aria-disabled="true">Disabled</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+
+    <nav class="cuthullo">
+    <ul class="nav justify-content">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PARA VOCÊ</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PARA INDÚSTRIA</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" aria-disabled="true">CULTURA</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" aria-disabled="true">EDUCAÇÃO</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" aria-disabled="true">ESPORTE</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" aria-disabled="true">EVENTOS</a>
+      </li>
+    </ul>
+
+    </nav>
+
+  </header>
+
