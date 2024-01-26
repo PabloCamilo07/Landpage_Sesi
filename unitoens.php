96ae304d0ae772aba8d6376ea5c0a207595cc341
Fhelipe Alves Mantovan <fhelipe.mantovan007@gmail.com>
2024-01-26 11:29:06 -0300

    trocar localização do php e css
    style.css foi incluido dentro da pasta css em seu próprio arquivo com nome customizado, quando unitoens.css for esvaziado troque em unitoens.php

--- unitoens.php
+++ unitoens.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <!-- ! renomear style!! -->
+    <link rel="stylesheet" href="css/unitoens-style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
@@ -20,33 +21,49 @@
                 <div class="col">
                     <div class="fam-location-container">
                         <img src="img/Rectangle 14.png" alt="166">
-                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional sesi</p>
+                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional <br>sesi</p>
                         <p class="fam-const-card-loc-number fam-const-uppercase-font">166</p>
                     </div>
                 </div>
                 <div class="col">
                     <div class="fam-location-container">
                         <img src="img/Rectangle 15.png" alt="166">
-                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional sesi</p>
+                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional <br>sesi</p>
                         <p class="fam-const-card-loc-number fam-const-uppercase-font">265</p>
                     </div>
                 </div>
                 <div class="col">
                     <div class="fam-location-container">
                         <img src="img/Rectangle 16.png" alt="166">
-                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional sesi</p>
+                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional  <br>sesi</p>
                         <p class="fam-const-card-loc-number fam-const-uppercase-font">221</p>
                     </div>
                 </div>
                 <div class="col">
                     <div class="fam-location-container">
                         <img src="img/Rectangle 18.png" alt="166">
-                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional sesi</p>
+                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional  <br>sesi</p>
                         <p class="fam-const-card-loc-number fam-const-uppercase-font">094</p>
                     </div>
                 </div>
-
             </div>
+        </div>
+
+        <div class="fam-noticias-container">
+            <p class="fam-topic-titles">Notícias</p>
+
+            <div class="fam-notcias-container-item col-md-2">
+                <img class="fam-noticias-image" src="img/WhatsApp Image 2024-01-25 at 13.39 1.png" alt="">
+
+                <p class="fam-noticias-text">Estudantes do SESI-166 vão a Brasília</p>
+            </div>
+            
+            <div class="fam-notcias-container-item col-md-2">
+                <img class="fam-noticias-image" src="img/WhatsApp Image 2024-01-25 at 13.39 1.png" alt="">
+
+                <p class="fam-noticias-text">Estudantes do SESI-166 vão a Brasília</p>
+            </div>
+
         </div>
         
     </div>
