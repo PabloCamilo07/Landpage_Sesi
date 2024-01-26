7a4fd37afb35234c18e371503282debc8e9ef646
PabloCamilo07 <pablo.aquino@aluno.senai.br>
2024-01-26 13:17:03 -0300

    carrossel de imagens inicial

--- begginpage.php
+++ begginpage.php
@@ -9,10 +9,9 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
   <!-- LINK BOOSTSTRAP JS-->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
   <link rel="stylesheet" href="style-begginpage.css">
-
   <title>Document</title>
 </head>
 
@@ -23,10 +22,10 @@
     <div id="carouselExample" class="carousel slide">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="img/imagem1.jpg" class="d-block " alt="">
+          <img src="imagem1.jpg" class="d-block w-100" alt="">
         </div>
         <div class="carousel-item">
-          <img src="img/imagem 2.jpg" class="d-block" alt="">
+          <img src="imagem2.jpg" class="d-block w-100" alt="">
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
