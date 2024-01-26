5fd5c1c2289b2cdf2df8fe661a48b6622b83d051
Fhelipe Alves Mantovan <fhelipe.mantovan007@gmail.com>
2024-01-26 11:05:34 -0300

    unidades até ensinos
    primeira atualização comicionada do unitoens, FAM

---
+++ templates/unitoens/unitoens.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="fam-page-margins">
+
+        <div class="fam-unidades-container">
+            <p class="fam-topic-titles">Unidades</p>
+
+            <div class="row fam-dflex-row">
+
+                <div class="col">
+                    <div class="fam-location-container">
+                        <img src="img/Rectangle 14.png" alt="166">
+                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional sesi</p>
+                        <p class="fam-const-card-loc-number fam-const-uppercase-font">166</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="fam-location-container">
+                        <img src="img/Rectangle 15.png" alt="166">
+                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional sesi</p>
+                        <p class="fam-const-card-loc-number fam-const-uppercase-font">265</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="fam-location-container">
+                        <img src="img/Rectangle 16.png" alt="166">
+                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional sesi</p>
+                        <p class="fam-const-card-loc-number fam-const-uppercase-font">221</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="fam-location-container">
+                        <img src="img/Rectangle 18.png" alt="166">
+                        <p class="fam-const-card-title fam-const-uppercase-font">Centro educacional sesi</p>
+                        <p class="fam-const-card-loc-number fam-const-uppercase-font">094</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        
+    </div>
+
+</body>
+
+</html>
